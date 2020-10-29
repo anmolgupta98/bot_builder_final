1d9df9fcdabf3b4d599ae70b056b9846589bc6ea
Anmol Gupta <f20171080@pilani.bits-pilani.ac.in>
2020-10-29 20:46:26 +0530

    Form added in bot settings

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -30,19 +30,19 @@ class BotsController < ApplicationController
       @bot = Bot.find(params[:id])
       if @bot.update(params.require(:botsettings).permit(:language, :initconv, :triggerpoint, :days))
          if params[:botsettings][:days] == "Everyday"
-            @bot.update(starttime: params[:bots][:startime_1])
-            @bot.update(endtime:  params[:bots][:endtime_1])
+            @bot.update(starttime: params[:bots][:startime1])
+            @bot.update(endtime:  params[:bots][:endtime1])
          elsif params[:botsettings][:days] == "Monday to Friday"
-            @bot.update(starttime:  params[:bots][:startime_2])
-            @bot.update(endtime:  params[:bots][:endtime_2])  
+            @bot.update(starttime:  params[:bots][:startime2])
+            @bot.update(endtime:  params[:bots][:endtime2])  
          elsif params[:botsettings][:days] == "Saturday and Sunday"
-            @bot.update(starttime:  params[:bots][:startime_3])
-            @bot.update(endtime:  params[:bots][:endtime_3])  
+            @bot.update(starttime:  params[:bots][:startime3])
+            @bot.update(endtime:  params[:bots][:endtime3])  
          else
-            @bot.update(starttime:  params[:bots][:startime_4])
-            @bot.update(endtime:  params[:bots][:endtime_4])  
-            @bot.update(startdate:  params[:bots][:startdate_4])        
-            @bot.update(enddate:  params[:bots][:enddate_4])   
+            @bot.update(starttime:  params[:bots][:startime4])
+            @bot.update(endtime:  params[:bots][:endtime4])  
+            @bot.update(startdate:  params[:bots][:startdate4])        
+            @bot.update(enddate:  params[:bots][:enddate4])   
          end
          @bot.update(params.require(:date).permit(:rebootconv)) 
          redirect_to composemessage_path 
@@ -59,11 +59,11 @@ class BotsController < ApplicationController
    end 
 
    def composemessage
-      @bot = Bot.where( user_id: current_user.id).last
+      @bot = Bot.find(params[:id])
    end
    
    def activatebot
-      @bot = Bot.where( user_id: current_user.id).last
+      @bot = Bot.find(params[:id])
    end
 
    def statistics
