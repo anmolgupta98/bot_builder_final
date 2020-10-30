fd8111a1cf7da8d653b7c16ea889060fcfb2dc35
Anmol Gupta <f20171080@pilani.bits-pilani.ac.in>
2020-10-30 15:31:30 +0530

    edit bot name

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -28,28 +28,35 @@ class BotsController < ApplicationController
     
     def update
       @bot = Bot.find(params[:id])
-      if @bot.update(params.require(:botsettings).permit(:language, :initconv, :triggerpoint, :days))
-         if params[:botsettings][:days] == "Everyday"
-            @bot.update(starttime: params[:bots][:startime1])
-            @bot.update(endtime:  params[:bots][:endtime1])
-         elsif params[:botsettings][:days] == "Monday to Friday"
-            @bot.update(starttime:  params[:bots][:startime2])
-            @bot.update(endtime:  params[:bots][:endtime2])  
-         elsif params[:botsettings][:days] == "Saturday and Sunday"
-            @bot.update(starttime:  params[:bots][:startime3])
-            @bot.update(endtime:  params[:bots][:endtime3])  
+      if params[:botsettings]
+         if @bot.update(params.require(:botsettings).permit(:language, :initconv, :triggerpoint, :days))
+            if params[:botsettings][:days] == "Everyday"
+               @bot.update(starttime: params[:bots][:startime1])
+               @bot.update(endtime:  params[:bots][:endtime1])
+            elsif params[:botsettings][:days] == "Monday to Friday"
+               @bot.update(starttime:  params[:bots][:startime2])
+               @bot.update(endtime:  params[:bots][:endtime2])  
+            elsif params[:botsettings][:days] == "Saturday and Sunday"
+               @bot.update(starttime:  params[:bots][:startime3])
+               @bot.update(endtime:  params[:bots][:endtime3])  
+            else
+               @bot.update(starttime:  params[:bots][:startime4])
+               @bot.update(endtime:  params[:bots][:endtime4])  
+               @bot.update(startdate:  params[:bots][:startdate4])        
+               @bot.update(enddate:  params[:bots][:enddate4])   
+            end
+            @bot.update(params.require(:date).permit(:rebootconv)) 
+            redirect_to composemessage_path 
          else
-            @bot.update(starttime:  params[:bots][:startime4])
-            @bot.update(endtime:  params[:bots][:endtime4])  
-            @bot.update(startdate:  params[:bots][:startdate4])        
-            @bot.update(enddate:  params[:bots][:enddate4])   
-         end
-         @bot.update(params.require(:date).permit(:rebootconv)) 
-         redirect_to composemessage_path 
-      else
-         flash[:alert] = "Couldn't save the bot details";
-         redirect_to composemessage_path
-      end 
+            flash[:alert] = "Couldn't save the bot details";
+            redirect_to composemessage_path
+         end 
+      end
+      if params[:botname]
+         if @bot.update(params.require(:botname).permit(:name))
+            flash.now[:notice] = "Name updated successfully"
+         end   
+      end   
     end    
 
    def destroy
